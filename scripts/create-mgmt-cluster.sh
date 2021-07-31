3a951e85806885ded02da914d259b77b1fb1adcb
Ryan Drew <learnitall0@gmail.com>
2021-07-31 17:09:22 -0600

    Use docker within VM rather than KVM driver

--- scripts/create-mgmt-cluster.sh
+++ scripts/create-mgmt-cluster.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
-# Create the management cluster within kvm
-# Assumes helm, minikube, argocd cli and kvm are installed
+# Create the management cluster
+# Assumes helm, minikube, podman, kvm and krew are installed
 # Note that this script is not idempotent
 
 set -xe
-minikube start --cpus=max --memory=max --driver=kvm --dns-domain='k8s.yaghl' --cni=flannel
 
-# Add extra interface
-minikube stop
-virsh --connect qemu:///system attach-interface minikube bridge br1 --target eth2 --config
-minikube start
+minikube start --driver=docker --dns-domain='k8s.yaghl' --cni=flannel
 
 # Add kubevirt
 minikube addons enable kubevirt
 
+# Install virtctl
+kubectl krew install virt
+
 # Install argocd
-minikube kubectl create namespace argocd
-minikube kubectl -- apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl create namespace argocd
+kubectl -- apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 # Install airflow
-minikube kubectl create namespace airflow
+kubectl create namespace airflow
 helm repo add apache-airflow https://airflow.apache.org
 helm install airflow apache-airflow/airflow --namespace airflow --wait --timeout 10m0s
