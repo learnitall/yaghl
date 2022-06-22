4d8801159843155f658e422d072fd23d12b94a1d
Ryan Drew <learnitall0@gmail.com>
2022-06-22 13:26:26 -0600

    Break out post install scripts
    Signed-off-by: Ryan Drew <learnitall0@gmail.com>

--- scripts/apply-cilium.sh
+++ scripts/apply-cilium.sh
@@ -3,10 +3,6 @@ set -xe
 
 KUBECONFIG=./data/k3s.yaml
 
-echo "Getting kubeconfig"
-rsync -avz --rsync-path="sudo rsync" -e "ssh -i ./data/sshkeys/id_rsa" core@192.168.3.121:/etc/rancher/k3s/k3s.yaml $KUBECONFIG
-sed -i 's/127.0.0.1/192.168.3.121/g' $KUBECONFIG
-
 echo "Installing cilium"
 cilium install
 
